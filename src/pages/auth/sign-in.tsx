57e11ff0f8662781e5a2ee87ef5a5eb8b609d009
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-25 18:16:54 -0300

    ADD: Implement toast notifications with Sonner
    - Sonner is a library for toast components in React
    - Implement success and error toasts for submitting the login form

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
@@ -20,8 +21,17 @@ export function SignIn() {
   } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
@@ -36,7 +46,11 @@ export function SignIn() {
             <p className="text-sm text-muted-foreground">
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
-            <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+            <form
+              id="form"
+              className="space-y-4"
+              onSubmit={handleSubmit(handleSignIn)}
+            >
               <div className="space-y-2">
                 <Label htmlFor="email">Seu e-mail</Label>
                 <Input id="email" type="email" {...register('email')} />
