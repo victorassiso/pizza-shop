51ce68a58671526919f8a7392c8a08a6a18738df
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-26 16:45:46 -0300

    ADD: 404 Not Found Page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,27 +2,33 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
-export const router = createBrowserRouter([
-  {
-    path: '/pizza-shop-web',
-    element: <AppLayout />,
-    children: [
-      { path: '/pizza-shop-web', element: <Dashboard /> },
-      { path: '/pizza-shop-web/orders', element: <Orders /> },
-    ],
-  },
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <AppLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { path: '/', element: <Dashboard /> },
+        { path: '/orders', element: <Orders /> },
+      ],
+    },
 
-  {
-    path: '/pizza-shop-web/',
-    element: <AuthLayout />,
-    children: [
-      { path: '/pizza-shop-web/sign-in', element: <SignIn /> },
-      { path: '/pizza-shop-web/sign-up', element: <SignUp /> },
-    ],
-  },
-])
+    {
+      path: '/',
+      element: <AuthLayout />,
+      errorElement: <NotFound />,
+      children: [
+        { path: '/sign-in', element: <SignIn /> },
+        { path: '/sign-up', element: <SignUp /> },
+      ],
+    },
+  ],
+  { basename: '/pizza-shop-web' },
+)
