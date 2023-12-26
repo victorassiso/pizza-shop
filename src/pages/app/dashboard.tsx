e1eb07e2f1ea2548d93ebaffadc650b39464cb03
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-25 21:49:46 -0300

    ADD: Implement Header with a nav bar

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,23 +1,10 @@
 import { Helmet } from 'react-helmet-async'
-import { Link } from 'react-router-dom'
-
-import { Button } from '@/components/ui/button'
 
 export function Dashboard() {
   return (
     <>
       <Helmet title="Dashboard" />
       <h1>Dashboard</h1>
-      <Button variant="ghost" asChild>
-        <Link to="/pizza-shop-web/sign-in" className="">
-          Sign-in
-        </Link>
-      </Button>
-      <Button variant="ghost" asChild>
-        <Link to="/pizza-shop-web/sign-up" className="">
-          Sign-up
-        </Link>
-      </Button>
     </>
   )
 }
---
+++ src/pages/app/orders.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Orders() {
+  return (
+    <>
+      <Helmet title="Orders" />
+      <h1>Orders</h1>
+    </>
+  )
+}
