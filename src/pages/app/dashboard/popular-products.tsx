5f76b2ef4c002e807514217a31b8cb9abcd441a7
Victor Assis <victor_assis@poli.ufrj.br>
2023-12-26 16:47:53 -0300

    PATCH: Remove unused dependencies

--- src/pages/app/dashboard/popular-products.tsx
+++ src/pages/app/dashboard/popular-products.tsx
@@ -1,13 +1,5 @@
 import { BarChart } from 'lucide-react'
-import {
-  Cell,
-  Pie,
-  PieChart,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
