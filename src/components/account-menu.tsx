e6e39753165bfe9f43c723407bdbdff8f9e9fac7
Victor Assis <victor_assis@poli.ufrj.br>
2024-01-04 12:19:04 -0300

    FEAT: LogOut

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -1,4 +1,5 @@
 import { Building, ChevronDown, LogOut } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 import { Button } from './ui/button'
 import {
@@ -33,10 +34,12 @@ export function AccountMenu() {
             <Building className="mr-2 h-4 w-4" />
             <span>Perfil da Loja</span>
           </DropdownMenuItem>
-          <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Sair</span>
-          </DropdownMenuItem>
+          <Link to="/sign-in">
+            <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+              <LogOut className="mr-2 h-4 w-4" />
+              <span>Sair</span>
+            </DropdownMenuItem>
+          </Link>
         </DropdownMenuLabel>
       </DropdownMenuContent>
     </DropdownMenu>
